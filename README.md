6266a21c355e1afa833cecd8429557e1e25adf5c
Landon West <landonwest5@gmail.com>
2016-07-19 14:26:06 -0700

    create readme, add numbers 0..9 to method

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ruby Password Generator
+This is a simple random string generator that shuffles upper and lowercase english alphabet characters, numbers 0-9, and a few common symbols.
+
+## Getting started
+
+include the module and call Password.gen in your code. 
--- rand_pw.rb
+++ rand_pw.rb
@@ -2,8 +2,9 @@ module Password
   def Password.gen(n = 16)
     up = ('A'..'Z').to_a
     down = ('a'..'z').to_a
+    num = (0..9).to_a
     sym = %w[! # $ % & ?]
-    pw = (up + down + sym).shuffle
+    pw = (up + down + num + sym).shuffle
     pw[0..(n-1)].join
   end
 end
